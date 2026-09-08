b68c3a93263cedc865830ee4a5f9708b1b71304f
Burthgulash <kvido.redl@gmail.com>
2026-09-08 21:33:38 +0200

    ok

--- scripts/script-generace-kalendare/kalendar-data.js
+++ scripts/script-generace-kalendare/kalendar-data.js
@@ -11,7 +11,7 @@ export const data = [
             "a další",
         ],
         obrazek: "https://burthgulash.github.io/kvido%20html-img/foto/Chalupa.jpg",
-        link: "#",
+        link: "https://burthgulash.github.io/Chynick_LARP/za-linii/za-linii.html",
         misto: "Pernink, kostelní 11 - apartmánová chalupa"
     },
     {
